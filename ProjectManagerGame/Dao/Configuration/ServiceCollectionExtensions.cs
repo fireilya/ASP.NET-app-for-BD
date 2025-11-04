58bbf2077b64e1ec9482c0d25cb20bab213e6541
Daniil <sgktost1@gmail.com>
2025-11-04 14:47:37 +0500

    Проект для взаимодействия с базой и внешними сервисами

---
+++ ProjectManagerGame/Dao/Configuration/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Core.EFCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dao.Configuration;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddDao(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddSingleton<IDbContextModelConfigurator, DbContextModelConfigurator>()
+           .AddTransient<ITestRepository, TestRepository>();
+
+        return serviceCollection;
+    }
+}
