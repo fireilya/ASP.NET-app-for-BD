baf07f0e264f7f05cb04d4ead2f4b9ea8e7b8766
Daniil <sgktost1@gmail.com>
2025-11-26 20:48:03 +0500

    Добавил две сущности для быстрого старта

---
+++ Core/EFCore/PostgresDataType.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.EFCore;
+
+public static class PostgresDataType
+{
+    public const string Guid = "uuid";
+    public const string Enum = "enum";
+    public const string Bool = "bolean";
+    public const string Text = "text";
+    public const string Int = "integer";
+    public const string Short = "smallint";
+    public const string Float = "real";
+    public const string Json = "jsonb";
+}
---
+++ Domain/Enumerations/GameEventType.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Enumerations;
+
+public enum GameEventType
+{
+    MoveAcceleration = 1,
+    EdgeBreak = 2,
+    Neutralize = 3,
+    Simple = 4,
+    InteractionAcceleration = 5,
+    Teleportation = 6,
+}
