172465388acb6d8b0266b53d9c197d3a5c325fee
Daniil <sgktost1@gmail.com>
2025-11-28 23:08:39 +0500

    Указывать тип данных вовсе не обязательно)
    EF Core очень умный, сам всё понимает. Но вот Json всё равно нужно указывать

--- Dao/Dao/Entities/PlayerSettingsDbo.cs
+++ Dao/Dao/Entities/PlayerSettingsDbo.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Core.EFCore;
 using Domain.Enumerations;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,21 +10,21 @@ namespace Dao.Entities;
 [Index(nameof(PlayerId), IsUnique = true)]
 public class PlayerSettingsDbo
 {
-    [Column("id", TypeName = PostgresDataType.Guid), Key]
+    [Column("id"), Key]
     public required Guid Id { get; set; }
 
-    [Column("player_id", TypeName = PostgresDataType.Guid)]
+    [Column("player_id")]
     public required Guid PlayerId { get; set; }
 
     [Column("screen_mode")]
     public required GameScreenMode EventType { get; set; }
 
-    [Column("music_volume", TypeName = PostgresDataType.Short), Range(0, 100)]
+    [Column("music_volume"), Range(0, 100)]
     public required short MusicVolume { get; set; }
 
-    [Column("sfx_volume", TypeName = PostgresDataType.Short), Range(0, 100)]
+    [Column("sfx_volume"), Range(0, 100)]
     public required short SfxVolume { get; set; }
 
-    [Column("text_speed", TypeName = PostgresDataType.Short), Range(0, 100)]
+    [Column("text_speed"), Range(0, 100)]
     public required short TextSpeed { get; set; }
 }
