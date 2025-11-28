83aad4175acd460825a0d79aba7254b2ff40bf26
Daniil <sgktost1@gmail.com>
2025-11-28 18:46:42 +0500

    Очепятка

--- Core/EFCore/PostgresDataType.cs
+++ Core/EFCore/PostgresDataType.cs
@@ -3,8 +3,7 @@
 public static class PostgresDataType
 {
     public const string Guid = "uuid";
-    public const string Enum = "enum";
-    public const string Bool = "bolean";
+    public const string Bool = "boolean";
     public const string Text = "text";
     public const string Int = "integer";
     public const string Short = "smallint";
