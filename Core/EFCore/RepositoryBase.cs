7bf21d707d72ec0cb1712fb783510699345fa771
Daniil <sgktost1@gmail.com>
2025-11-28 18:46:58 +0500

    Синтаксические ништяки

---
+++ Core/EFCore/RepositoryBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace Core.EFCore;
+
+public abstract class RepositoryBase<TDbo, TDto, TPrimaryKey>(
+    ISingletonDataContext dataContext,
+    IEntityConverter<TDbo, TDto> converter
+)
+    where TDbo : class
+    where TDto : class
+{
+    public Task CreateAsync(TDto dto) => dataContext.InsertAsync(converter.ToDbo(dto));
+
+    public async Task<TDto?> FindAsync(TPrimaryKey key)
+    {
+        var foundDbo = await dataContext.FindAsync<TDbo, TPrimaryKey>(key);
+        return foundDbo is null ? null : converter.ToDto(foundDbo);
+    }
+}
