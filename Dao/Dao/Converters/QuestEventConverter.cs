046f7eea27e3280ff14f7945b9401b63512b5b6e
Daniil <sgktost1@gmail.com>
2025-12-03 16:47:57 +0500

    QuestEventDto

---
+++ Dao/Dao/Converters/QuestEventConverter.cs
@@ -0,0 +1,22 @@
+﻿using Core.EFCore;
+using Dao.Entities;
+using Domain.FlattenDtos;
+
+namespace Dao.Converters;
+
+public class QuestEventConverter : IEntityConverter<QuestEventDbo, QuestEventDto>
+{
+    public QuestEventDbo ToDbo(QuestEventDto dto) => new()
+    {
+        Id = dto.Id,
+        EventId = dto.EventId,
+        ResearchAreaId = dto.ResearchAreaId,
+    };
+
+    public QuestEventDto ToDto(QuestEventDbo dbo) => new()
+    {
+        Id = dbo.Id,
+        EventId = dbo.EventId,
+        ResearchAreaId = dbo.ResearchAreaId,
+    };
+}
---
+++ Dao/Dao/Entities/QuestEventDbo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Dao.Entities;
+
+[Table("node")]
+public class QuestEventDbo
+{
+    [Column("id"), Key]
+    public required Guid Id { get; set; }
+
+    [Column("event_id")]
+    public required Guid EventId { get; set; }
+
+    [Column("research_area_id")]
+    public required Guid ResearchAreaId { get; set; }
+}
