416e72b68a3f05ccfa6ff8812ddb0f98937dfb64
Daniil <sgktost1@gmail.com>
2025-12-03 16:43:49 +0500

    ResearchArea

---
+++ Domain/FlattenDtos/ResearchAreaDto.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Domain.FlattenDtos;
+
+public record ResearchAreaDto
+{
+    public required Guid Id { get; set; }
+    public required string PathToTexture { get; set; }
+}
