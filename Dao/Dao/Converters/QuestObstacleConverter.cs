0000e3970911f0546524b5ad4f676028a31631a8
Daniil <sgktost1@gmail.com>
2025-12-03 16:31:01 +0500

    QuestObstacle

---
+++ Dao/Dao/Converters/QuestObstacleConverter.cs
@@ -0,0 +1,30 @@
+﻿using Core.EFCore;
+using Dao.Entities;
+using Domain.FlattenDtos;
+
+namespace Dao.Converters;
+
+public class QuestObstacleConverter : IEntityConverter<QuestObstacleDbo, QuestObstacleDto>
+{
+    public QuestObstacleDbo ToDbo(QuestObstacleDto dto) => new()
+    {
+        Id = dto.Id,
+        ResearchAreaId = dto.ResearchAreaId,
+        Name = dto.Name,
+        Description = dto.Description,
+        TimeDelayInMinutes = dto.TimeDelayInMinutes,
+        MaxInstance = dto.MaxInstance,
+        PathToIcon = dto.PathToIcon,
+    };
+
+    public QuestObstacleDto ToDto(QuestObstacleDbo dbo) => new()
+    {
+        Id = dbo.Id,
+        ResearchAreaId = dbo.ResearchAreaId,
+        Name = dbo.Name,
+        Description = dbo.Description,
+        TimeDelayInMinutes = dbo.TimeDelayInMinutes,
+        MaxInstance = dbo.MaxInstance,
+        PathToIcon = dbo.PathToIcon,
+    };
+}
---
+++ Domain/FlattenDtos/QuestObstacleDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Domain.FlattenDtos;
+
+public record QuestObstacleDto
+{
+    public required Guid Id { get; set; }
+    public required Guid ResearchAreaId { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public required short TimeDelayInMinutes { get; set; }
+    public required short MaxInstance { get; set; }
+    public required string PathToIcon { get; set; }
+}
