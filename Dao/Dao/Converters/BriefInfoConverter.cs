ec45911d4779d15f26ebaa05741bda2388bff62e
SmthOnYourMind <mastaden46@gmail.com>
2025-12-08 19:38:19 +0500

    brief_info added

---
+++ Dao/Dao/Converters/BriefInfoConverter.cs
@@ -0,0 +1,26 @@
+﻿using Core.EFCore;
+using Dao.Entities;
+using Domain.FlattenDtos;
+
+namespace Dao.Converters;
+
+public class BriefInfoConverter : IEntityConverter<BriefInfoDbo, BriefInfoDto>
+{
+    public BriefInfoDbo ToDbo(BriefInfoDto dto) => new()
+    {
+        Id = dto.Id,
+        QuestId = dto.QuestId,
+        IsTrue = dto.IsTrue,
+        AffectedEntityId = dto.AffectedEntityId,
+        Content = dto.Content,
+    };
+
+    public BriefInfoDto ToDto(BriefInfoDbo dbo) => new()
+    {
+        Id = dbo.Id,
+        QuestId = dbo.QuestId,
+        IsTrue = dbo.IsTrue,
+        AffectedEntityId = dbo.AffectedEntityId,
+        Content = dbo.Content,
+    };
+}
