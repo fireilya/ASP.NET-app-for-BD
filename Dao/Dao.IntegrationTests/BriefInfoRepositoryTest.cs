4f413d94829c7f23ede16ac651a46cfdcffcf800
SmthOnYourMind <mastaden46@gmail.com>
2025-12-08 19:38:19 +0500

    brief_info added

---
+++ Dao/Dao.IntegrationTests/BriefInfoRepositoryTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using AutoFixture;
+using Dao.Entities;
+using Dao.Repositories;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using TestCore.IntegrationTests;
+
+namespace Dao.IntegrationTests;
+
+public class BriefInfoRepositoryTest : IntegrationTestBase
+{
+    private IBriefInfoRepository Repository => ServiceProvider.GetRequiredService<IBriefInfoRepository>();
+
+    [Test]
+    public async Task TestFind_WhenEntityExist()
+    {
+        // Arrange
+        var briefInfoDbo = Fixture.Create<BriefInfoDbo>();
+        await DataContext.InsertAsync(briefInfoDbo);
+
+        // Act
+        var briefInfoDto = await Repository.FindAsync(briefInfoDbo.Id);
+
+        // Assert
+        briefInfoDto.Should().NotBeNull();
+        briefInfoDto.Should().BeEquivalentTo(briefInfoDbo);
+    }
+}
---
+++ Dao/Dao/Repositories/IBriefInfoRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Core.EFCore;
+using Dao.Entities;
+using Domain.FlattenDtos;
+
+namespace Dao.Repositories;
+
+public interface IBriefInfoRepository : IRepository
+{
+    Task CreateAsync(BriefInfoDto briefInfoDto);
+    Task<BriefInfoDto?> FindAsync(Guid briefInfoId);
+    Task<BriefInfoDto[]> SelectAsync(Guid[] briefInfoIds);
+    Task UpdateAsync(BriefInfoDto briefInfoDto);
+    Task DeleteAsync(BriefInfoDto briefInfoDto);
+}
+
+public class BriefInfoRepository(
+    ISingletonDataContext dataContext,
+    IEntityConverter<BriefInfoDbo, BriefInfoDto> converter
+) : RepositoryBase<BriefInfoDbo, BriefInfoDto, Guid>(dataContext, converter, x => x.Id), IBriefInfoRepository
+{
+}
