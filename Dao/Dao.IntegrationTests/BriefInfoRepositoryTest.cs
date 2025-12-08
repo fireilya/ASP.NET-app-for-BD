f0a1b82a1bd3f97ea55598a5710fc56d9fc3ad49
SmthOnYourMind <mastaden46@gmail.com>
2025-12-08 19:38:19 +0500

    brief_info commit

---
+++ Dao/Dao.IntegrationTests/BriefInfoRepositoryTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using AutoFixture;
+using Dao.Entities;
+using Dao.Repositories;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using TestCore.IntegrationTests;
+
+namespace Dao.IntegrationTests;
+
+public class BriefInfoRepositoryTest : IntegrationTestBase
+{
+    private IBriefInfoRepository Repository => ServiceProvider.GetRequiredService<IBriefInfoRepository>();
+
+    [Test]
+    public async Task TestRead_WhenEntityExist()
+    {
+        // Arrange
+        var briefInfoDbo = Fixture.Create<BriefInfoDbo>();
+        await DataContext.InsertAsync(briefInfoDbo);
+
+        // Act
+        var briefInfoDto = await Repository.ReadAsync(briefInfoDbo.Id);
+
+        // Assert
+        briefInfoDto.Should().NotBeNull();
+        briefInfoDto.Should().BeEquivalentTo(briefInfoDbo);
+    }
+}
