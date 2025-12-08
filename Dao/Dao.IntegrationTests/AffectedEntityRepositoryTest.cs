67a14939d2e0bd6a343cd23b347bd52028e8b65b
SmthOnYourMind <mastaden46@gmail.com>
2025-12-08 20:45:20 +0500

    affected_entity commit

---
+++ Dao/Dao.IntegrationTests/AffectedEntityRepositoryTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using AutoFixture;
+using Dao.Entities;
+using Dao.Repositories;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using TestCore.IntegrationTests;
+
+namespace Dao.IntegrationTests;
+
+public class AffectedEntityRepositoryTest : IntegrationTestBase
+{
+    private IAffectedEntityRepository Repository => ServiceProvider.GetRequiredService();
+    [Test]
+    public async Task TestRead_WhenEntityExist()
+    {
+        // Arrange
+        var affectedEntityDbo = Fixture.Create();
+        await DataContext.InsertAsync(affectedEntityDbo);
+        
+        // Act
+        var affectedEntityDto = await Repository.ReadAsync(affectedEntityDbo.Id);
+        
+        // Assert
+        affectedEntityDto.Should().NotBeNull();
+        affectedEntityDto.Should().BeEquivalentTo(affectedEntityDbo);
+    }
+}
---
+++ Dao/Dao/Converters/AffectedEntityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Core.EFCore;
+using Dao.Entities;
+using Domain.FlattenDtos;
+
+namespace Dao.Converters;
+
+public class AffectedEntityConverter : IEntityConverter<AffectedEntityDbo, AffectedEntityDto>
+{
+    public AffectedEntityDbo ToDbo(AffectedEntityDto dto) => new()
+    {
+        Id = dto.Id,
+        QuestId = dto.QuestId,
+        AffectedEntityType = dto.AffectedEntityType,
+    };
+    public AffectedEntityDto ToDto(AffectedEntityDbo dbo) => new()
+    {
+        Id = dbo.Id,
+        QuestId = dbo.QuestId,
+        AffectedEntityType = dbo.AffectedEntityType,
+    };
+}
