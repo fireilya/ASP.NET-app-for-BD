aecf4d6178cad55e745048023c180cf096012c0a
elsu4kinesss <krolikrohla@gmail.com>
2025-12-09 21:14:31 +0500

    update quest_workers

--- Domain/FlattenDtos/QuestWorkersDto.cs
+++ Domain/FlattenDtos/QuestWorkersDto.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.FlattenDtos;
-public class QuestWorkerDbo
+public class QuestWorkersDbo
 {
     public required Guid Id { get; set; }
     public required Guid WorkerId { get; set; }
