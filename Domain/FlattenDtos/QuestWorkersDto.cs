c872a2ac8dde09186ac496e85107159aee082cb4
elsu4kinesss <krolikrohla@gmail.com>
2025-12-09 21:10:16 +0500

    quest_workers

---
+++ Domain/FlattenDtos/QuestWorkersDto.cs
@@ -0,0 +1,11 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.FlattenDtos;
+public class QuestWorkerDbo
+{
+    public required Guid Id { get; set; }
+    public required Guid WorkerId { get; set; }
+    public required Guid QuestId { get; set; }
+}
