942d9dd61cc8fd378eced36068ed3650eccb3966
chetinav <v_nika@bk.ru>
2025-12-15 00:33:54 +0500

    update tool

--- Domain/FlattenDtos/ToolDto.cs
+++ Domain/FlattenDtos/ToolDto.cs
@@ -5,6 +5,5 @@ namespace Domain.FlattenDtos;
 public class ToolDto
 {
     public required Guid Id { get; set; }
-    public required Guid ResourceId { get; set; }
     public required string PathToIcon { get; set; }
 }
