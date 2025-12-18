3e0d4c55c96bfb540b080e1d12b8d83c1a161d36
Daniil <sgktost1@gmail.com>
2025-12-18 23:14:20 +0500

    tool controller

---
+++ GameService/Controllers/ToolController.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Scheduler;
+using GameService.Storages;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameService.Controllers;
+
+[ApiController]
+[Route("tools")]
+public class ToolController(
+    IToolStorage toolStorage
+) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> CreateTool([FromBody] Tool tool)
+    {
+        await toolStorage.SaveAsync(tool);
+        return Ok();
+    }
+
+    [HttpGet(nameof(toolId))]
+    public async Task<IActionResult> GetTool([FromRoute] Guid toolId)
+    {
+        var tool = await toolStorage.FindAsync(toolId);
+        if (tool is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(tool);
+    }
+}
