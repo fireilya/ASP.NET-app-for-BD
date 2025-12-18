9b428e4d647a5bf896ed82ffd460502b7e44cac8
Daniil <sgktost1@gmail.com>
2025-12-18 20:33:12 +0500

    Переношу стораджи в сервис

--- GameService/Storages/IActionAreaStorage.cs
+++ GameService/Storages/IActionAreaStorage.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Dao.Repositories;
 using Domain.FlattenDtos;
 using Domain.Scheduler;
 
-namespace Dao.Storages;
+namespace GameService.Storages;
 
 public interface IActionAreaStorage
 {
